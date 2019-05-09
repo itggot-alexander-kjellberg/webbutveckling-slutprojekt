479cdfe5546d8bc110231cfc9eb8175603f0c3a8
itggot-alexander-kjellberg <alexander.kjellberg2@elev.ga.ntig.se>
2019-05-09 14:59:59 +0200

    Har fixat snygga transitions till hamburgermenyn, har bytt document titel

--- site/js/main.js
+++ site/js/main.js
@@ -1,7 +1,7 @@
 function visaHamburger(event){
-    let nunna = document.querySelector("#menun")
-    nunna.classList.toggle("nunna")
-
     let fadeLayer = document.querySelector(".fade-layer")
     fadeLayer.classList.toggle("visible")
+
+    let delay = document.querySelector("#menun")
+    delay.classList.toggle("delay")
 }
